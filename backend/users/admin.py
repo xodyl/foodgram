887a8c3a0f1fe62e2593616bcdf3928575c813e6
Boris Potapov <borisportretov@gmail.com>
2024-11-03 15:43:14 +0000

    add admin

---
+++ backend/users/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from api.models import Recipe
+
+
+User = get_user_model()
+
+
+class RecipeInline(admin.StackedInline):
+    model = Recipe
+    extra = 0
+
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    inlines = (RecipeInline,)
+    list_display = (
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'role',
+        'email',
+        'avatar'
+    )
+    search_fields = ('user', 'email',)
+    list_filter = ('role',)
+    list_editable = (
+        'role',
+    )
